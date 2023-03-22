9645d85f9639c80236387e392826f413e576df75
Justin Delille <justind7513@gmail.com>
2023-03-21 18:14:30 -0700

    wip: setting up game cards

--- app/components/GameCard/GameCard.tsx
+++ app/components/GameCard/GameCard.tsx
@@ -3,15 +3,38 @@
 import React from 'react';
 import './GameCard.scss';
 import { Game } from '@/app/types/Game';
+import Image from 'next/image';
 
 interface GameCardProps {
 	game: Game;
 }
 
+
 export default function GameCard(props: GameCardProps) {
+
+  const {competitions: [{competitors}]} = props.game;
+  const homeLogo = competitors[0].team.logo;
+  const homeName = competitors[0].team.abbreviation
+
+  const awayLogo = competitors[1].team.logo;
+  const awayName = competitors[1].team.abbreviation
+
+
 	return (
-		<div className='card'>
-			<div>
+		<div className='card flex-col gap'>
+			<div className="home flex gap">
+        <Image alt='logo' src={homeLogo} width={30} height={30} />
+        <p>{homeName}</p>
+      </div>
+      <div className="away flex gap">
+      <Image alt='logo' src={awayLogo} width={30} height={30} />
+      <p>{awayName}</p>
+      </div>
+		</div>
+	);
+}
+
+{/* <div>
 				{props.game.homeTeam} vs. {props.game.awayTeam}
 			</div>
 			<div>
@@ -19,7 +42,6 @@ export default function GameCard(props: GameCardProps) {
 			</div>
 			<div>{props.game.name}</div>
 			<div>{props.game.shortName}</div>
-			<div>{props.game.teamLink}</div>
-		</div>
-	);
-}
+			<div>
+        <Image src={homeLogo} alt="logo" width={50} height={50}/>
+      </div> */}
--- app/types/Game.ts
+++ app/types/Game.ts
@@ -5,5 +5,43 @@ export type Game = {
  awayScore: number;
  name: string;
  shortName: string;
- teamLink: string;
-}
+ id: string;
+ date: string;
+ competitions: {
+   id: string;
+   status: {
+     type: string;
+     detail: string;
+   };
+   competitors: [
+     {
+       id: string;
+       name: string;
+       score: string;
+       homeAway: string;
+       team: {
+         abbreviation: string;
+         logo: string;
+       };
+     },
+     {
+       id: string;
+       name: string;
+       score: string;
+       homeAway: string;
+       team: {
+         abbreviation: string;
+         logo: string;
+       };
+     }
+   ];
+   venue: {
+     fullName: string;
+     address: {
+       city: string;
+       state: string;
+       country: string;
+     };
+   };
+ }[];
+};
